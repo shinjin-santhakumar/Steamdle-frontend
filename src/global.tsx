7b248d4a4d3a6e53073d6d3ab7ac1da96e9ec38f
Shinjin Santhakumar <shinjinoliver@gmail.com>
2026-07-12 11:57:35 -0700

    trailing char removal

--- src/global.tsx
+++ src/global.tsx
@@ -1,6 +1,6 @@
 //const server = "http://127.0.0.1:5000"; //local python env pyhonanywhere
-//const server = "http://127.0.0.1:5001/"; //vercel python flask-app
+//const server = "http://127.0.0.1:5001"; //vercel python flask-app
 
 //const server = "https://shinjinsos.pythonanywhere.com";
-const server = "https://steamdle-backend-4gr4.vercel.app/";
+const server = "https://steamdle-backend-4gr4.vercel.app";
 export default server;
