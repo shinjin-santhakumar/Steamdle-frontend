b422ee96ef6a93ba9ffce5bbbe8199fb37f6ffa4
Shinjin Santhakumar <shinjinoliver@gmail.com>
2026-07-12 11:17:57 -0700

    server update

--- src/global.tsx
+++ src/global.tsx
@@ -1,4 +1,6 @@
-//const server = "http://127.0.0.1:5000";
+//const server = "http://127.0.0.1:5000"; //local python env pyhonanywhere
+//const server = "http://127.0.0.1:5001/"; //vercel python flask-app
 
-const server = "https://shinjinsos.pythonanywhere.com";
+//const server = "https://shinjinsos.pythonanywhere.com";
+const server = "https://steamdle-backend-4gr4.vercel.app/";
 export default server;
